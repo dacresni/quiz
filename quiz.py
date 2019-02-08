2c08e926377530afbe7a119b9d28d5a83db3f483
Nehemiah I. Dacres <vivacarlie@gmail.com>
2019-02-08 16:38:42 -0600

    now it's an API

--- quiz.py
+++ quiz.py
@@ -1,18 +1,20 @@
-import json, http.client
+import json, requests, unittest
+from flask import Flask, jsonify
 DOMAIN= "api.github.com"
+
+@app.route("/followers/<username>")
 def followers(github_id):
     follower_ids = []
     """Write an API endpoint that accepts a GitHub ID and returns Follower GitHub ID’s (up to 5 Followers total) associated with the passed in GitHub ID.  Retrieve data up to 3 levels deep, repeating the process of retrieving Followers (up to 5 Followers total) for each Follower found.  Data should be returned in JSON format """
     def pull(id,num):
         if num <= 0:
-          return follower_ids
+          return jsonify(follower_ids)
         num -= 1
         followers = requests.get("/users/{githubid}/followers".format(github_id)).json()
         for user in followers[0:5]:
           id = user["login"]
           follower_ids.append()
           pull(id,num)
-    
     pull(id,3)
     
 def bounus(github_id):
